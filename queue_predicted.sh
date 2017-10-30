fdf8bd0f02f3a73c9397e826e678303202df0934
Kevin Tran <ktrain9891@gmail.com>
2017-10-30 16:59:36 -0400

    Update Luigi host, surrogate model, and narrow the target

--- queue_predicted.sh
+++ queue_predicted.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32
-#SBATCH --time=2:00:00
-#SBATCH --partition=regular
-#SBATCH --job-name=vasp
-#SBATCH --output=queuepredicted-%j.out
-#SBATCH --error=queuepredicted-%j.error
-#SBATCH --constraint=haswell
 
 module load python
 cd /global/project/projectdirs/m2755/GASpy/GASpy_feedback
@@ -15,12 +7,15 @@ source activate /project/projectdirs/m2755/GASpy_conda/
 PYTHONPATH='.' luigi \
     --module feedback Predictions \
     --ads-list '["CO"]' \
+    --prediction-min -2.5 \
+    --prediction-max 1.5 \
     --prediction-target -0.55 \
-    --model-location '/global/project/projectdirs/m2755/GASpy/GASpy_regressions/pkls/GP_around_TPOT_FEATURES_coordcount_ads_neighbors_coordcounts_RESPONSES_energy_BLOCKS_.pkl' \
+    --model-location '/global/project/projectdirs/m2755/GASpy/GASpy_regressions/pkls/GP_around_TPOT_FEATURES_coordcount_neighbors_coordcounts_RESPONSES_energy_BLOCKS_adsorbate.pkl' \
     --priority 'gaussian' \
+    --block '("CO",)' \
     --xc 'rpbe' \
     --max-submit 200 \
-    --scheduler-host 128.55.224.41 \
+    --scheduler-host 128.55.224.51 \
     --workers=4 \
     --log-level=WARNING \
     --parallel-scheduling \
