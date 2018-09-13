726fe0917e8fa267474b61b3b788480a9e53c149
Kevin Tran <ktrain9891@gmail.com>
2018-09-13 10:47:36 -0400

    Make Docker image

---
+++ open_container_via_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# This script will use Docker to start an interactive container to run GASpy.
+# Note that you need to run this script from the directory that it is in.
+
+
+# Establish out how to mount GASpy to the container. This is the part
+# that assumes that you are running this script inside GASpy.
+gaspy_feedback_path=$(pwd)
+gaspy_path="$(dirname "$gaspy_feedback_path")"
+gaspy_mounting_config="$gaspy_path:/home/GASpy"
+
+# Create a container from the image
+#   -it     run interactively
+#   --rm    close the container when we exit
+#   -w      The container directory to start in
+#   -v      mount various things to the container
+docker run -it --rm -w "/home" \
+    -v $gaspy_mounting_config \
+    ulissigroup/gaspy_feedback:0.20 \
+    /bin/bash
---
+++ open_container_via_shifter.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This script will use Docker to start an interactive container to run GASpy.
+# Note that you need to run this script from the directory that it is in.
+
+
+# Establish out how to mount GASpy to the container. This is the part
+# that assumes that you are running this script inside GASpy.
+gaspy_feedback_path=$(pwd)
+gaspy_path="$(dirname "$gaspy_feedback_path")"
+gaspy_mounting_config="$gaspy_path:/home/GASpy"
+
+# Now open the container
+shifter --image=ulissigroup/gaspy_feedback:0.20 --volume=$gaspy_mounting_config bash -i
